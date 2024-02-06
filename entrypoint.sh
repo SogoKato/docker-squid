a06227a1ffe719347279bc9700fa06d8296f0436
Sogo Kato <sogo.kato@outlook.com>
2024-02-06 23:27:09 +0900

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,10 +16,10 @@ create_log_dir
 create_cache_dir
 
 # allow arguments to be passed to squid
-if [[ ${1:0:1} = '-' ]]; then
+if [ "${1:0:1}" = '-' ]; then
   EXTRA_ARGS="$@"
   set --
-elif [[ ${1} == squid || ${1} == $(which squid) ]]; then
+elif [ "$1" = squid ] || [ "$1" = "$(which squid)" ]; then
   EXTRA_ARGS="${@:2}"
   set --
 fi
